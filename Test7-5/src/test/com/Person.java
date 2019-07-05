7a681024057f4ac1661d45eb8cfde5d74fe42d7c
Administrator <Administrator@SD-20190630ILNI>
2019-07-05 20:20:10 +0800

    测试

---
+++ Test7-5/src/test/com/Person.java
@@ -0,0 +1,12 @@
+package test.com;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("姓名：曾远洋");
+		System.out.println("志向：赚很多很多的钱！");
+		System.out.println("课工场欢迎您！");
+	}
+
+}
