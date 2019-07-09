f584348d2ff4a9420ac05aa907788abc640b88ff
Administrator <Administrator@SD-20190630ILNI>
2019-07-09 19:52:09 +0800

    2019-7-9

---
+++ Test7-9/src/com/cn/schedule.java
@@ -0,0 +1,23 @@
+package com.cn;
+
+public class schedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * @author zyy
+		 * @version 1.0
+		 */
+		System.out.println("----------------------------------------");
+		System.out.println("星期          早上                   下午                      ");
+		System.out.println("----------------------------------------");
+		System.out.println("星期一      英语                   高数                       ");
+		System.out.println("星期二      政治                   计算机网络            ");
+		System.out.println("星期三      C++           Java            ");
+		System.out.println("星期四      数据结构            Java            ");
+		System.out.println("星期五      数据库               操作系统               ");
+		System.out.println("----------------------------------------");
+		
+	}
+
+}
