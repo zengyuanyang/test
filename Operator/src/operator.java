68daad0cc0df0fbe91dcfac6055eeacfe37829fc
Administrator <Administrator@SD-20190630ILNI>
2019-07-14 16:46:38 +0800

    运算符

---
+++ Operator/src/operator.java
@@ -0,0 +1,18 @@
+/**
+ * @version 运算符
+ * @author 曾远洋
+ *
+ */
+public class operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5&6);  //按位与
+		System.out.println(5|6);  //按位或
+		System.out.println(5^6);  //按位异或
+		System.out.println(6<<2); //左移
+		System.out.println(12>>2);//右移
+		System.out.println(15>8?15:8); //条件运算符
+	}
+
+}
