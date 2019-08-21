6af68dd307e0a0708e4f87fa5a58ad8e7c3266c0
zengyuanyang <531857704@qq.com>
2019-08-21 20:23:52 +0800

    21日晚更新

--- Homework/src/HomeWork8_21.java
+++ Homework/src/HomeWork8_21.java
@@ -23,13 +23,36 @@ public class HomeWork8_21 {
 		/*
 		maxmin();	//调用maxmin方法
         */	
+		/*
 		reversed_order();	//调用reversed_order方法
-		}
-	public static void day(int i) {	//根据输入的索引值，返回字符串数组对应的元素
+        */
+		/*
+		sum_array();	//调用sum_array方法
+        */	
+		/*
+		System.out.println("请输入杨辉三角的行数：");
+		int h = sc.nextInt();
+		pascal_triangle(h);	//调用Pascal_triangle方法
+        */	
+		
+		System.out.println("请输入一串小于八位数的数据：");
+		int mima = sc.nextInt();
+		encryption(mima);	//调用encryption方法
+		
+	}
+	
+	
+	
+	//根据输入的索引值，返回字符串数组对应的元素
+	public static void day(int i) {	
 		String[] str = {"星期天","星期一","星期二","星期三","星期四","星期五","星期六"};
 		System.out.println("对应元素为：" + str[i]);
 	}
-	public static void search(int i) {	//寻找值在数组中的对应位子
+	
+	
+	
+	//寻找值在数组中的对应位子
+	public static void search(int i) {	
 		int[] array = {1,3,6,12,8,6,9};
 		int count = 0;
 		for(int j = 0;j < array.length;j++) {
@@ -43,7 +66,11 @@ public class HomeWork8_21 {
 			System.out.println("数组中没有该数据！");
 		}
 	}
-	public static void maxmin() {	//输出数组中的最大值和最小值
+	
+	
+	
+	//输出数组中的最大值和最小值
+	public static void maxmin() {	
 		int[] array = {2,8,13,6,22,64,1,24};
 		int max = array[0];
 		int min = array[0];
@@ -57,7 +84,11 @@ public class HomeWork8_21 {
 		}
 		System.out.println("该数组最大值为：" + max + "，最小值为：" + min);
 	}
-	public static void reversed_order() {	//将数组逆序输出
+	
+	
+	
+	//将数组逆序输出
+	public static void reversed_order() {	
 		int[] array = {1,3,8,9,14,16,5};
 		System.out.print("数组正序输出为：[");
 		
@@ -83,6 +114,85 @@ public class HomeWork8_21 {
 				System.out.print(array[i] + ",");
 			}
 		}
-		
+	}
+	
+	
+	
+	//二维数组所有元素求和
+	public static void sum_array() {
+		int[][] sale = {{22,66,44},{77,33,88},{25,45,65},{11,66,99}};
+		int sum = 0;
+		for(int i = 0;i < sale.length;i++) {
+			for(int j = 0;j < sale[i].length;j++) {
+				sum = sum + sale[i][j];
+			}
+		}
+		System.out.println("本年度的销售额为：" + sum);
+	}
+	
+	
+	
+	//输出杨辉三角（行数可键入）
+	public static void pascal_triangle(int i) {
+		int[][] array = new int[i][i];
+		//将杨辉三角的数据录入二维数组
+		array[0][0] = 1;
+		for(int j = 1;j < array.length;j++) {
+			array[j][0] = 1;
+			for(int k = 1;k < array[j].length;k++) {
+				array[j][k] = array[j-1][k] + array[j-1][k-1];
+			}
+		}
+		//输出杨辉三角
+		for(int j = 0;j < array.length;j++) {
+			for(int k = 0;k < array[j].length;k++) {
+				if(array[j][k] != 0) {//元素不为0时，输出元素值
+					System.out.print(array[j][k]);
+					System.out.print('\t');
+				}
+			}
+			System.out.print('\n');	//每行输出结束后换行
+		}
+	}
+	
+	
+	
+	//数据加密
+	public static void encryption(int mima) {
+		int weishu = 0;
+		int i = 1;
+		//计算数据的位数
+		while(1 == 1) {
+			if(mima/i != 0) {
+				weishu++;
+				i *= 10;
+			}else {
+				break;
+			}
+		}
+		if(weishu >= 8) {//判断当数据位数大于等于8位时，提示错误
+			System.out.println("位数过多，请重新输入！");
+			return;
+		}
+		System.out.println("该数据为：" + weishu + "位数。");
+		int[] array = new int[weishu];	//定义数组
+		int k = 1;
+	    //将数据逆序存入数组
+		for(int j = 0;j < weishu;j++) {
+			array[j] = (mima/k)%10;
+			k *= 10;
+		}
+		//对数据进行加密处理
+		for(int j = 0;j < array.length;j++) {
+			array[j] = (array[j] + 5) % 10;
+		}
+		//首尾数据互换
+		k = array[0];
+		array[0] = array[weishu-1];	
+		array[weishu-1] = k;
+		System.out.print("加密后的数据为：");
+		for(int j = 0;j < array.length;j++) {	//遍历输出加密后的数据
+			System.out.print(array[j]);
+		}
 	}
 }
--- Program50/src/Program14_Day.java
+++ Program50/src/Program14_Day.java
@@ -12,13 +12,101 @@ public class Program14_Day {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("请输入年份：");
 		int year = sc.nextInt();
+		if(year <= 0) { //判断输入的年份是否正确
+			System.out.println("请正确输入年份！");
+			return;
+		}
 		System.out.println("请输入月份：");
 		int month = sc.nextInt();
+		if(month <= 0 || month > 12) {	//判断输入的月份是否正确
+			System.out.println("请正确输入月份！");
+			return;
+		}
 		System.out.println("请输入号数：");
 		int day = sc.nextInt();
-		if((year%4 == 0 && year%100 != 0) || year%400 == 0) {
+		if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {	//判断输入的号数是否正确
+			if(day < 1 || day > 31) {
+				System.out.println("请正确输入号数！");
+				return;
+			}
+		}else if(month == 4 || month == 6 || month == 9 || month == 11) {
+			if(day < 1 || day > 30) {
+				System.out.println("请正确输入号数！");
+				return;
+			}
 			
 		}
+		if((year%4 == 0 && year%100 != 0) || year%400 == 0) {
+			if(month == 2) {
+				if(day < 1 || day > 29) {
+					System.out.println("请正确输入号数！");
+					return;
+				}
+			}
+		}else {
+			if(month == 2) {
+				if(day < 1 || day > 28) {
+					System.out.println("请正确输入号数！");
+					return;
+				}
+			}
+		}
+		
+		
+		if((year%4 == 0 && year%100 != 0) || year%400 == 0) {//判断为闰年时
+			if(month >= 3) {	//闰年月份大于等于3时，天数加1
+				System.out.println("这天是本年的第" + (sumday(month,day)+1) + "天。");
+			}else {	//闰年的前两个月
+				System.out.println("这天是本年的第" + sumday(month,day) + "天。");
+			}
+			
+		}else {//为平年时
+			System.out.println("这天是本年的第" + sumday(month,day) + "天。");
+		}
+		
+	}
+	public static int sumday(int month,int day) {	//平年天数计算的方法，返回值为int类型的天数
+		int sum = 0;
+		switch(month) {	//利用switch
+		case 1:
+			sum = day;
+			break;
+		case 2:
+			sum = 31 + day;
+			break;
+		case 3:
+			sum = 59 + day;
+			break;
+		case 4:
+			sum = 90 + day;
+			break;
+		case 5:
+			sum = 120 + day;
+			break;
+		case 6:
+			sum = 151 + day;
+			break;
+		case 7:
+			sum = 181 + day;
+			break;
+		case 8:
+			sum = 212 + day;
+			break;
+		case 9:
+			sum = 243 + day;
+			break;
+		case 10:
+			sum = 273 + day;
+			break;
+		case 11:
+			sum = 304 + day;
+			break;
+		case 12:
+			sum = 334 + day;
+			break;
+		
+		}
+		return sum;
 	}
 
 }
